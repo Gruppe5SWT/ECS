483a47bc296a63449d4bd84c322003987f237b6d
MetteThorn <au640153@post.au.dk>
2021-09-14 09:55:20 +0200

    FakeHeater added

--- ECS.Redesign/Heater.cs
+++ ECS.Redesign/Heater.cs
@@ -18,6 +18,24 @@
         }
     }
 
+    public class FakeHeater : IHeater
+    {
+        public void TurnOn()
+        {
+            System.Console.WriteLine("Heater is on");
+        }
+
+        public void TurnOff()
+        {
+            System.Console.WriteLine("Heater is off");
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+
     public interface IHeater
     {
         void TurnOn();
