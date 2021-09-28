b5225b84ce7fa1bca4982784bbb3da0cbabdd2ab
Marc <marcbkonig@gmail.com>
2021-09-28 09:44:55 +0200

    comment in unit tests with NSubstitute

--- ECS.Redesign.Test.Unit/UnitTestsWithNSubstitute.cs
+++ ECS.Redesign.Test.Unit/UnitTestsWithNSubstitute.cs
@@ -22,51 +22,51 @@ namespace ECS.Redesign.Test.Unit
             uut = new ECS(23, heater, tempSensor);
         }
 
-        //[Test]
-        //public void ECS_InitialiseECS_ThresholdIsExpected()
-        //{
-        //    Assert.That(uut.GetThreshold().Equals(23));
-        //}
+        [Test]
+        public void ECS_InitialiseECS_ThresholdIsExpected()
+        {
+            Assert.That(uut.GetThreshold().Equals(23));
+        }
 
-        //[Test]
-        //public void Regulate_TempUnderThreshold_HeaterTurnsOn()
-        //{
-        //    tempSensor.GetTemp().Returns(20);
-        //    uut.Regulate();
-        //    heater.Received(1).TurnOn();
-        //}
+        [Test]
+        public void Regulate_TempUnderThreshold_HeaterTurnsOn()
+        {
+            tempSensor.GetTemp().Returns(20);
+            uut.Regulate();
+            heater.Received(1).TurnOn();
+        }
 
-        //[Test]
-        //public void Regulate_TempOverThreshold_HeaterTurnsOff()
-        //{
-        //    tempSensor.GetTemp().Returns(25);
-        //    uut.Regulate();
-        //    heater.Received(1).TurnOff();
-        //}
+        [Test]
+        public void Regulate_TempOverThreshold_HeaterTurnsOff()
+        {
+            tempSensor.GetTemp().Returns(25);
+            uut.Regulate();
+            heater.Received(1).TurnOff();
+        }
 
-        //[TestCase(21)]
-        //[TestCase(24)]
-        //[TestCase(25)]
-        //[TestCase(27)]
-        //[TestCase(0)]
-        //[TestCase(-1)]
-        //public void SetThreshold_SetNew_GetReturnsExcepted(int temp)
-        //{
-        //    uut.SetThreshold(temp);
-        //    Assert.That(uut.GetThreshold().Equals(temp));
-        //}
+        [TestCase(21)]
+        [TestCase(24)]
+        [TestCase(25)]
+        [TestCase(27)]
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void SetThreshold_SetNew_GetReturnsExcepted(int temp)
+        {
+            uut.SetThreshold(temp);
+            Assert.That(uut.GetThreshold().Equals(temp));
+        }
 
-        //[TestCase(21)]
-        //[TestCase(24)]
-        //[TestCase(25)]
-        //[TestCase(27)]
-        //[TestCase(0)]
-        //[TestCase(-1)]
-        //public void GetCurTemp_GetCurrentTemp_TempSensorReturnsExpected(int temp)
-        //{
-        //    tempSensor.GetTemp().Returns(temp);
-        //    Assert.That(uut.GetCurTemp().Equals(temp));
-        //}
+        [TestCase(21)]
+        [TestCase(24)]
+        [TestCase(25)]
+        [TestCase(27)]
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void GetCurTemp_GetCurrentTemp_TempSensorReturnsExpected(int temp)
+        {
+            tempSensor.GetTemp().Returns(temp);
+            Assert.That(uut.GetCurTemp().Equals(temp));
+        }
 
         [TestCase(true, true, true)]
         [TestCase(true, false, false)]
@@ -79,23 +79,23 @@ namespace ECS.Redesign.Test.Unit
             Assert.That(uut.RunSelfTest().Equals(uutTest));
         }
 
-        //[Test]
-        //public void RunSelfTest_RunTest_TestsReturnFalse()
-        //{
-        //    tempSensor.RunSelfTest().Returns(false);
-        //    heater.RunSelfTest().Returns(true);
-        //    Assert.That(uut.RunSelfTest().Equals(false));
-        //}
+        [Test]
+        public void RunSelfTest_RunTest_TestsReturnFalse()
+        {
+            tempSensor.RunSelfTest().Returns(false);
+            heater.RunSelfTest().Returns(true);
+            Assert.That(uut.RunSelfTest().Equals(false));
+        }
 
-        //[Test]
-        //public void Regulate_HeaterOffTemperatureGoesLow_HeaterTurnsOn()
-        //{
-        //    heater.TurnOff();
-        //    tempSensor.GetTemp().Returns(20);
-        //    uut.Regulate();
+        [Test]
+        public void Regulate_HeaterOffTemperatureGoesLow_HeaterTurnsOn()
+        {
+            heater.TurnOff();
+            tempSensor.GetTemp().Returns(20);
+            uut.Regulate();
 
-        //    heater.Received(1).TurnOn();
-        //    heater.Received(1).TurnOff();
-        //}
+            heater.Received(1).TurnOn();
+            heater.Received(1).TurnOff();
+        }
     }
 }
